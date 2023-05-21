2f493a6eb44395af65e792a8c45de8509613170e
stefanbrkic1 <stefanbrkicdzn.contact@gmail.com>
2023-05-21 10:36:36 +0200

    Made code more readable

--- script.js
+++ script.js
@@ -18,82 +18,82 @@ firstNameSpan.classList.remove('first-name-input')
 lastNameSpan.classList.remove('last-name-input')
 
 
-firstNameInput.addEventListener('input', ()=>{
+firstNameInput.addEventListener('input', () => {
     let firstName = firstNameInput.value;
     let containsNumbers = /\d/.test(firstName);
-    if(containsNumbers){
+    if (containsNumbers) {
         firstNameSpan.classList.add('first-name-input')
         firstNameInput.classList.add('error')
     }
-    else{
+    else {
         firstNameSpan.classList.remove('first-name-input')
         firstNameInput.classList.remove('error')
     }
 })
 
 
-lastNameInput.addEventListener('input', ()=>{
-        let lastName = lastNameInput.value;
-        let containsNumbers = /\d/.test(lastName);
-        if(containsNumbers){
-            lastNameSpan.classList.add('last-name-input')
-            lastNameInput.classList.add('error')
-        }
-        else{
-            lastNameSpan.classList.remove('last-name-input')
-            lastNameInput.classList.remove('error')
-        }
+lastNameInput.addEventListener('input', () => {
+    let lastName = lastNameInput.value;
+    let containsNumbers = /\d/.test(lastName);
+    if (containsNumbers) {
+        lastNameSpan.classList.add('last-name-input')
+        lastNameInput.classList.add('error')
+    }
+    else {
+        lastNameSpan.classList.remove('last-name-input')
+        lastNameInput.classList.remove('error')
+    }
 })
 
 
-emailInput.addEventListener('blur', () =>{
-    if(emailInput.value===""){
-       
+emailInput.addEventListener('blur', () => {
+    if (emailInput.value === "") {
+
     }
-    else{
-        if(!emailInput.value.includes('@')){
+    else {
+        if (!emailInput.value.includes('@')) {
             emailInput.setCustomValidity('Please enter a valid email.')
         }
-        else{
+        else {
             emailInput.setCustomValidity('');
         }
         emailInput.reportValidity();
     }
-    
+
 })
 
-telInput.addEventListener('blur', () =>{
-    if(telInput.value===""){
-        
+telInput.addEventListener('blur', () => {
+    if (telInput.value === "") {
+
     }
-    else{
-        if(isNaN(telInput.value)){
+    else {
+        if (isNaN(telInput.value)) {
             telInput.setCustomValidity('Please enter a valid phone number')
         }
-        else{
+        else {
             telInput.setCustomValidity('');
         }
         telInput.reportValidity();
     }
 })
 
-password1.addEventListener('input', ()=>{
-    if(password1.value.length<8){
+password1.addEventListener('input', () => {
+    if (password1.value.length < 8) {
         password1.classList.add('error')
         passwordMessage1.classList.add('password1-input');
     }
-    else{
+    else {
         password1.classList.remove('error')
         passwordMessage1.classList.remove('password1-input');
     }
 })
 
-password2.addEventListener('input', ()=>{
-    if(password1.value!==password2.value){
+password2.addEventListener('input', () => {
+    if (password1.value !== password2.value) {
         password2.classList.add('error')
         passwordMessage2.classList.add('password2-input');
     }
-    else{
+    else {
         password2.blur();
         password2.classList.remove('error')
         passwordMessage2.classList.remove('password2-input');
@@ -101,53 +101,53 @@ password2.addEventListener('input', ()=>{
 })
 
 
-submitBtn.addEventListener('click', ()=>{
+submitBtn.addEventListener('click', () => {
     checkFirstName();
     checkLastName();
 })
 
-function checkFirstName(){
+function checkFirstName() {
     let firstName = firstNameInput.value;
     let containsNumbers = /\d/.test(firstName);
-    if(containsNumbers){
+    if (containsNumbers) {
         firstNameInput.setCustomValidity('Please enter a valid name(Without 1,2,3,+,*,-...)')
-        firstNameInput.value='';
+        firstNameInput.value = '';
     }
-    else{
+    else {
         firstNameInput.setCustomValidity('')
     }
     firstNameInput.reportValidity();
 }
 
-function checkLastName(){
+function checkLastName() {
     let lastName = lastNameInput.value;
     let containsNumbers = /\d/.test(lastName);
-    if(containsNumbers){
+    if (containsNumbers) {
         lastNameInput.setCustomValidity('Please enter a valid last name(Without 1,2,3,+,*,-...)')
-        lastNameInput.value='';
+        lastNameInput.value = '';
     }
-    else{
+    else {
         lastNameInput.setCustomValidity('')
     }
     lastNameInput.reportValidity();
 }
 
-function checkPassword(){
-    if(password1.value!==password2.value){
+function checkPassword() {
+    if (password1.value !== password2.value) {
         password1.setCustomValidity('Please enter a valid last name(Without 1,2,3,+,*,-...)')
     }
-    else{
-        
+    else {
+
     }
 }
 
 
-inputs.forEach(function(input){
-    input.addEventListener('blur', ()=>{
-        if(input.validity.valid){
+inputs.forEach(function (input) {
+    input.addEventListener('blur', () => {
+        if (input.validity.valid) {
             input.classList.add('input-valid')
         }
-        else{
+        else {
             input.classList.remove('input-valid')
         }
     })
